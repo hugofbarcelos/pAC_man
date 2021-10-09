643d9930cd48585d2b9be222a8a5ade6d49e8d7d
Code Cadet <codecadet@vonneumann.local>
2021-10-09 17:33:58 +0000

    add pac-man prenda version

---
+++ pac-man-prenda/src/org/academiadecodigo/altcatras/pacman/MainTests.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.altcatras.pacman;
+
+public class MainTests {
+
+    public static void main(String[] args) {
+
+        Field field = new Field();
+
+        field.createField();
+
+    }
+}
